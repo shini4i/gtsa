ed7ea5111aed07b45ee48823c3a96c40fabac02c
Vadim Gedz <github@shini4i.dev>
2025-10-13 13:25:59 +0300

    chore: add nix setup (#25)

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Development flake for gitlab-token-scope-adjuster";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        nodejs = pkgs.nodejs_22;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            nodejs
+            pkgs.nodePackages.ts-node
+          ];
+
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+            export NPM_CONFIG_FUND=0
+            export NPM_CONFIG_AUDIT=0
+
+            if [ ! -d node_modules ]; then
+              echo "Installing npm dependencies using package-lock.json..."
+              npm ci
+            fi
+          '';
+        };
+      });
+}
