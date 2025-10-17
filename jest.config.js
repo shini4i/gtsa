ef7b5eaebd64b91bbc8d0e0152bb9bb5764f6888
Vadim Gedz <github@shini4i.dev>
2025-10-17 19:57:45 +0300

    bump deps

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,23 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: false,
   coverageDirectory: 'coverage',
   clearMocks: true,
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest', {
+      jsc: {
+        parser: {
+          syntax: 'typescript',
+          tsx: true,
+          decorators: true,
+        },
+        transform: {
+          decoratorMetadata: true,
+        },
+        target: 'es2022',
+      },
+      sourceMaps: true,
+    }],
+  },
 };
